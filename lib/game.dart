c2a6e69d118e60c45a477eb8d9dc4a045bca0775
LuanFabricio <luanlimaleite@hotmail.com>
2024-06-12 22:16:30 -0300

    fix: Removing useless logs, comments and privating the board var

--- lib/game.dart
+++ lib/game.dart
@@ -1,17 +1,9 @@
-import 'package:flutter/material.dart';
-
 class TicTacToe {
-  ///  3 estados -> vazio, x, o
-  ///  00 -> vazio
-  ///  01 -> x
-  ///  10 -> o
-  ///  11 -> vazio (no futuro pode indicar se a o jogador ganhou ou nao
-  ///  9  2bits = 18 bits => 2 bytes 2 bits => 3 bytes)
-
-  /// 0 -> 0b11
-  /// 1 -> 0b11 << 2
-  // Uint8List board = Uint8List(3);
-  int board = 0;
+  /// Every 2 bits have 3 states
+  /// 01 -> x
+  /// 10 -> o
+  /// 00, 11 -> Empty
+  int _board = 0;
   static const xBits = 1;
   static const oBits = 2;
 
@@ -26,10 +18,7 @@ class TicTacToe {
   }
 
   int getValue(int index) {
-    int val = (board & _indexMask(index)) >> _indexShift(index);
-
-    var boardStr = board.toRadixString(2).padLeft(32, "0");
-    debugPrint("Board: 0b$boardStr");
+    int val = (_board & _indexMask(index)) >> _indexShift(index);
     return val;
   }
 
@@ -47,12 +36,12 @@ class TicTacToe {
   }
 
   bool insert(int index, int value) {
-    int currentValue = board & _indexMask(index);
+    int currentValue = _board & _indexMask(index);
     if (currentValue == xBits || currentValue == oBits) {
       return false;
     }
 
-    board |= value << _indexShift(index);
+    _board |= value << _indexShift(index);
     return true;
   }
 
@@ -67,64 +56,45 @@ class TicTacToe {
 
   /// index: Column index [0, 1, 2].
   bool columnWin(int columnIndex) {
-    /*
-       01 00 00
-       01 00 00
-       01 00 00
-
-       pivot = 01 = (board & 11)
-       if pivot == (00 | 11) return false;
-
-       mask = (pivot) | (pivot << (2*1)) | (pivot << (2*2))
-       return (mask & board) != 0;
-     */
-    int pivot = (board & _indexMask(columnIndex)) >> _indexShift(columnIndex);
+    int pivot = (_board & _indexMask(columnIndex)) >> _indexShift(columnIndex);
     if (pivot != oBits && pivot != xBits) return false;
 
     int mask = (pivot) << _indexShift(columnIndex) |
         (pivot << _indexShift(columnIndex + 3)) |
         (pivot << _indexShift(columnIndex + 6));
-    var boardStr = board.toRadixString(2).padLeft(32, "0");
-    debugPrint("[Col] Board:\t\t0b$boardStr");
-    var maskStr = mask.toRadixString(2).padLeft(32, "0");
-    debugPrint("[Col] Mask:\t\t0b$maskStr");
-    return (board & mask) == mask;
+    return (_board & mask) == mask;
   }
 
   /// index: Row index [0, 1, 2].
   bool rowWin(int rowIndex) {
     int index = rowIndex * 3;
-    int pivot = (board & _indexMask(index)) >> _indexShift(index);
+    int pivot = (_board & _indexMask(index)) >> _indexShift(index);
     if (pivot != oBits && pivot != xBits) return false;
 
     int mask = (pivot) << _indexShift(index) |
         (pivot << _indexShift(index + 1)) |
         (pivot << _indexShift(index + 2));
-    var boardStr = board.toRadixString(2).padLeft(32, "0");
-    debugPrint("[Row] Board:\t\t0b$boardStr");
-    var maskStr = mask.toRadixString(2).padLeft(32, "0");
-    debugPrint("[Row] Mask:\t\t0b$maskStr");
-    return (board & mask) == mask;
+    return (_board & mask) == mask;
   }
 
   bool diagonalWin() {
     int index = 0;
-    int pivot = (board & _indexMask(index)) >> _indexShift(index);
+    int pivot = (_board & _indexMask(index)) >> _indexShift(index);
     bool won = pivot != oBits && pivot != xBits;
 
     int mask = (pivot << _indexShift(index)) |
         (pivot << _indexShift(index + 4)) |
         (pivot << _indexShift(index + 8));
-    won = !won && ((mask & board) == mask);
+    won = !won && ((mask & _board) == mask);
     if (won) return true;
 
     index = 2;
-    pivot = (board & _indexMask(index)) >> _indexShift(index);
+    pivot = (_board & _indexMask(index)) >> _indexShift(index);
     if (pivot != oBits && pivot != xBits) return false;
     mask = (pivot << _indexShift(index)) |
         (pivot << _indexShift(index + 2)) |
         (pivot << _indexShift(index + 4));
 
-    return (mask & board) == mask;
+    return (mask & _board) == mask;
   }
 }
