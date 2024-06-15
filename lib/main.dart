ae14179454c7cff6876a833400b0235ae05bb5bc
LuanFabricio <luanlimaleite@hotmail.com>
2024-06-15 13:07:48 -0300

    feat: Integrating game module on flutter App and adding winner popup on the end of game

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe_app/game.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,6 +23,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool isX = false;
   List<String> board = List<String>.filled(9, '', growable: false);
+  var tictactoe = TicTacToe();
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +45,7 @@ class _HomePageState extends State<HomePage> {
                             border: Border.all(color: Colors.grey)),
                         child: Center(
                             child: Text(
-                          board[index],
+                          TicTacToe.valueToString(tictactoe.getValue(index)),
                           style: const TextStyle(
                             color: Colors.white,
                             fontSize: 40,
@@ -54,93 +56,57 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> _onRefresh() async {
+    _refresh();
+  }
+
+  void _refresh() {
     setState(() {
-      board = List<String>.filled(9, '', growable: false);
+      isX = false;
+      tictactoe.reset();
     });
   }
 
   void _tapped(int index) {
     setState(() {
-      String piece = isX ? "x" : "o";
+      final piece = isX ? TicTacToe.xBits : TicTacToe.oBits;
 
-      if (_placePiece(index, piece)) {
+      if (tictactoe.insert(index, piece)) {
         isX = !isX;
       }
 
-      int winner = _haveSomeOneWon();
-      if (winner != -1) {
-        board = board.map((e) => winner.toString()).toList();
+      if (_haveSomeOneWon()) {
+        final winner = isX ? 'O' : 'X';
+        debugPrint("Winner: $winner");
+
+        showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+                  title: const Text("We have a winner!"),
+                  content: Text("The $winner player has won!"),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          _refresh();
+                          Navigator.pop(context);
+                        },
+                        child: const Text("Ok")),
+                  ],
+                ));
       }
     });
   }
 
-  bool _placePiece(int index, String piece) {
-    if (board[index].compareTo("") == 0) {
-      board[index] = piece;
+  bool _haveSomeOneWon() {
+    if (tictactoe.columnWin(0) ||
+        tictactoe.columnWin(1) ||
+        tictactoe.columnWin(2)) {
       return true;
     }
-    return false;
-  }
 
-  int _haveSomeOneWon() {
-    int winner = isX ? 0 : 1;
-
-    if (_haveSameOnLine(0) || _haveSameOnLine(3) || _haveSameOnLine(6)) {
-      return winner;
+    if (tictactoe.rowWin(0) || tictactoe.rowWin(1) || tictactoe.rowWin(2)) {
+      return true;
     }
 
-    if (_haveSameOnColumn(0) || _haveSameOnColumn(1) || _haveSameOnColumn(2)) {
-      return winner;
-    }
-
-    if (_haveSameOnDiagonalLeftRight() || _haveSameOnDiagonalRightLeft()) {
-      return winner;
-    }
-
-    return -1;
-  }
-
-  bool _haveSameOnLine(int init) {
-    bool line = true;
-    int s, e;
-    String cs, ce;
-    for (int i = 0; i < 2; i++) {
-      s = i + init;
-      e = i + init + 1;
-      cs = board[s];
-      ce = board[e];
-      line &= cs.compareTo(ce) == 0;
-    }
-
-    bool isNotEmpty = board[init].compareTo("") != 0;
-    return line && isNotEmpty;
-  }
-
-  bool _haveSameOnColumn(int init) {
-    bool column = true;
-    int s, e;
-    String cs, ce;
-    for (int i = 0; i < 2; i++) {
-      s = i * 3 + init;
-      e = (i + 1) * 3 + init;
-      cs = board[s];
-      ce = board[e];
-      column &= cs.compareTo(ce) == 0;
-    }
-
-    bool isNotEmpty = board[init].compareTo("") != 0;
-    return column && isNotEmpty;
-  }
-
-  bool _haveSameOnDiagonalLeftRight() {
-    return board[0].compareTo("") != 0 &&
-        board[0].compareTo(board[4]) == 0 &&
-        board[0].compareTo(board[8]) == 0;
-  }
-
-  bool _haveSameOnDiagonalRightLeft() {
-    return board[2].compareTo("") != 0 &&
-        board[2].compareTo(board[4]) == 0 &&
-        board[2].compareTo(board[6]) == 0;
+    return tictactoe.diagonalWin();
   }
 }
